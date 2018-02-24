ccf024b64acb5cb8412e1e9c64bc592c52d26cb7
AJRohrer <andrewjrohrer@gmail.com>
2018-02-24 10:36:47 -0600

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  directives: [HeaderComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'App works!';
 }
